02a3e4e10adb39e1efac29eab27ca06a44a9d4f9
Ronambulo <enriquerodridel@gmail.com>
2024-01-29 14:19:40 +0100

    prueb

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
+
+    // menu hamburguesa
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
